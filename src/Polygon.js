602711729ad1c35177d9c026f0c084e4c1ab14de
Mathias Hansen <me@codemonkey.io>
2019-02-02 20:47:23 -0500

    Polygon rendering is working now

--- src/Polygon.js
+++ src/Polygon.js
@@ -1,9 +1,22 @@
 import { defaultPolygonOptions } from './defaultOptions';
+import TileConversion from './TileConversion';
+import { geoPath, geoTransform } from 'd3-geo';
+import * as topojson from 'topojson-client';
+
+const POLYGON_CACHE = {};
 
 export default class Polygon {
   constructor(sourceUrl, options = {}) {
     this._sourceUrl = sourceUrl;
     this._options = Object.assign({}, defaultPolygonOptions, options);
+    this._geometry = null;
+
+    fetch(this._sourceUrl)
+      .then(response => response.json())
+      .then(json => {
+        this._geometry = json;
+      })
+      .catch(err => console.log(`Could not load ${this._sourceUrl}: ${err.message || err}`));
   }
 
   get sourceUrl() {
@@ -15,36 +28,57 @@ export default class Polygon {
   }
 
   render(context, mapState) {
+    if (!this._geometry) {
+      return;
+    }
+
     context.fillStyle = this.options.color;
     context.strokeStyle = this.options.color;
 
-    this.renderPolygon(context);
+    this.mapState = mapState;
+
+    const center = [
+      this.mapState.canvasDimensions[0] / 2,
+      this.mapState.canvasDimensions[1] / 2
+    ];
+
+    const transform = geoTransform({point: this.projectPoint, mapState, center });
+    const path = geoPath(transform).context(context);
+
+    context.beginPath();
+    path(topojson.mesh(this._geometry));
+    context.stroke();
   }
 
-  renderPolygon(context) {
-    /*
-    const markerWidth = 17.698069;
-    const markerHeight = 24.786272;
+  projectPoint(x, y) {
+    const cachedPosition = (x, y, mapState) => {
+      const cacheKey = JSON.stringify([
+        [y, x], this.mapState.center, this.mapState.zoom,
+        this.mapState.tileSize, this.mapState.canvasDimensions
+      ]);
 
-    const x = data[0] - markerWidth / 2;
-    const y = data[1] - markerHeight;
+      if (cacheKey in POLYGON_CACHE) {
+        return POLYGON_CACHE[cacheKey];
+      }
 
-    context.save();
-    context.transform(0.184386, 0.000000, 0.000000, 0.184386, 0.551658 + x, 4.095760 + y);
-    context.beginPath();
-    context.lineWidth = 1.667195;
-    context.moveTo(45.000000, -22.212949);
-    context.bezierCurveTo(18.494941, -22.212949, -2.991863, -0.726145, -2.991863, 25.778914);
-    context.bezierCurveTo(-2.991863, 52.282306, 45.000000, 112.212950, 45.000000, 112.212950);
-    context.bezierCurveTo(45.000000, 112.212950, 92.991863, 52.282306, 92.991863, 25.777247);
-    context.bezierCurveTo(92.991863, -0.726145, 71.505059, -22.212949, 45.000000, -22.212949);
-    context.moveTo(45.000000, 43.827962);
-    context.bezierCurveTo(33.553042, 43.827962, 24.273437, 34.550024, 24.273437, 23.103067);
-    context.bezierCurveTo(24.273437, 11.656109, 33.553042, 2.376504, 45.000000, 2.376504);
-    context.bezierCurveTo(56.446958, 2.376504, 65.726563, 11.654442, 65.726563, 23.101399);
-    context.bezierCurveTo(65.726563, 34.548357, 56.446958, 43.827962, 45.000000, 43.827962);
-    context.fill();
-    context.restore();
-    */
+      const position = TileConversion.latLonToPixel(
+        [y, x],
+        this.mapState.center,
+        this.mapState.zoom,
+        this.mapState.tileSize,
+        this.mapState.canvasDimensions
+      );
+
+      POLYGON_CACHE[cacheKey] = position;
+
+      return position;
+    };
+
+    const position = cachedPosition(x, y, this.mapState);
+
+    this.stream.point(
+      this.center[0] - position[0] + this.mapState.moveOffset[0],
+      this.center[1] - position[1] + this.mapState.moveOffset[1]
+    );
   }
 }
