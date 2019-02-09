40a11a62ec03c81c287915240dbcdacc11fcb561
Mathias Hansen <me@codemonkey.io>
2019-02-08 23:06:40 -0500

    Working on polygon performance

--- src/Polygon.js
+++ src/Polygon.js
@@ -1,83 +1,120 @@
 import { defaultPolygonOptions } from './defaultOptions';
 import TileConversion from './TileConversion';
-import { geoPath, geoTransform } from 'd3-geo';
 import { feature } from 'topojson-client';
 
 export default class Polygon {
-  constructor(sourceUrl, objectName, options = {}) {
-    this._sourceUrl = sourceUrl;
-    this._objectName = objectName;
+  constructor(json, objectName, options = {}) {
     this._options = Object.assign({}, defaultPolygonOptions, options);
-    this._geometry = null;
 
-    fetch(this._sourceUrl)
-      .then(response => response.json())
-      .then(json => {
-        this._geometry = feature(json, json.objects[this._objectName]);
-      })
-      .catch(err => console.log(`Could not load ${this._sourceUrl}: ${err.message || err}`));
-  }
-
-  get sourceUrl() {
-    return this._sourceUrl;
+    window._geometry = feature(json, json.objects[objectName]);
   }
 
   get options() {
     return this._options;
   }
 
+  getGeometry() {
+    return window._geometry;
+  }
+
+  getProjectedGeometry() {
+    return window._projectedGeometry;
+  }
+
   render(context, mapState) {
-    if (!this._geometry) {
+    if (!this.getGeometry()) {
       return;
     }
 
     context.fillStyle = this.options.color;
     context.strokeStyle = this.options.color;
 
-    if (!this._projectedGeometry) {
-      this._projectedGeometry = this._geometry.features.map(feature => {
+    // Do we need to reproject the geometry?
+    if (this.buildState(mapState) !== window._projectedGeometryState) {
+      window._projectedGeometry = null;
+    }
+
+    if (!this.getProjectedGeometry()) {
+      window._projectedGeometry = this.getGeometry().features.map(feature => {
         return {
           ...feature,
-          geometry: this.projectGeometry(feature.geometry)
+          geometry: this.projectGeometry(feature.geometry, mapState)
         };
       });
+
+      window._projectedGeometryState = this.buildState(mapState);
     }
 
-    if (!this.path) {
-      this.mapState = mapState;
-
-      const center = [
-        this.mapState.canvasDimensions[0] / 2,
-        this.mapState.canvasDimensions[1] / 2
-      ];
-
-      const transform = geoTransform({point: this.projectPoint, mapState, center });
-
-      this.path = geoPath(transform).context(context);
-    }
+    const center = [
+      mapState.canvasDimensions[0] / 2,
+      mapState.canvasDimensions[1] / 2
+    ];
 
     context.beginPath();
-    this.path(this._geometry);
+    this.getProjectedGeometry().map(item => {
+      item.geometry.map(list => {
+        const coordinatesInsideViewPort = list.filter(position => {
+          const coordinate = this.offsetCoordinate(position, center, mapState.moveOffset);
+
+          return (coordinate[0] > 0 && coordinate[1] > 0
+            && coordinate[0] <= mapState.canvasDimensions[0] && coordinate[1] <= mapState.canvasDimensions[1]);
+        });
+
+        if (coordinatesInsideViewPort.length > 0) {
+          list.map((position, index) => {
+            const coordinate = this.offsetCoordinate(position, center, mapState.moveOffset);
+
+            if (index === 0) {
+              context.moveTo(coordinate[0], coordinate[1]);
+            } else {
+              context.lineTo(coordinate[0], coordinate[1]);
+            }
+          });
+        }
+      });
+    });
+
     context.fill();
     context.stroke();
   }
 
-  projectGeometry(geometry) {
-    console.log(geometry);
+  offsetCoordinate(position, center, moveOffset) {
+    return [
+      center[0] - position[0] + moveOffset[0],
+      center[1] - position[1] + moveOffset[1]
+    ];
   }
 
-  projectPoint(mapState, center, x, y) {
-    const position = TileConversion.latLonToPixel(
+  projectGeometry(geometry, mapState) {
+    switch (geometry.type) {
+      case 'Polygon':
+        return [geometry.coordinates[0].map(item => this.projectPoint(mapState, item[0], item[1]))];
+
+      case 'MultiPolygon':
+        return geometry.coordinates.map(coordinates =>
+          coordinates[0].map(item => this.projectPoint(mapState, item[0], item[1]))
+        );
+    }
+
+    return [];
+  }
+
+  projectPoint(mapState, x, y) {
+    return TileConversion.latLonToPixel(
       [y, x],
       mapState.center,
       mapState.zoom,
       mapState.tileSize,
       mapState.canvasDimensions
     );
+  }
 
-    const projectedX = center[0] - position[0] + mapState.moveOffset[0];
-    const projectedY = center[1] - position[1] + mapState.moveOffset[1];
-
-    return [projectedY, projectedX];
+  buildState(mapState) {
+    return JSON.stringify([
+      mapState.center,
+      mapState.zoom,
+      mapState.tileSize,
+      mapState.canvasDimensions
+    ]);
   }
 }
