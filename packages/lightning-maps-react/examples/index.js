e000c2da89d3a0620f60f3ee9f2421ee49aa4aaf
Mathias Hansen <me@codemonkey.io>
2019-02-19 10:12:50 -0500

    Fixed issue with rendering examples

--- packages/lightning-maps-react/examples/index.js
+++ packages/lightning-maps-react/examples/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import AppPerformance from './AppPerformance'
+import App from './App'
 
-ReactDOM.render(<AppPerformance />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
