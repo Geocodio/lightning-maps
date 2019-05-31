1548e4032b5d41a6789abad94f3d4d1e35a87843
Mathias Hansen <me@codemonkey.io>
2019-05-30 22:50:48 -0400

    Setting tile images to crossOrigin: anonymous now

--- src/TileLayer.js
+++ src/TileLayer.js
@@ -93,6 +93,7 @@ export default class TileLayer {
       this.map.state.tiles[tile.id] = new Image();
       this.map.state.tiles[tile.id].tileId = tile.id;
       this.map.state.tiles[tile.id].src = tileUrl;
+      this.map.state.tiles[tile.id].crossOrigin = 'anonymous';
       this.map.state.tiles[tile.id].loaded = false;
       this.map.state.tiles[tile.id].onload = () => {
         this.map.state.tiles[tile.id].loaded = true;
